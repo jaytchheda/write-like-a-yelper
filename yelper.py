5708726f249fea013867510a75ea38a44f899f91
Jay C <jaytchheda@gmail.com>
2018-05-21 05:42:35 -0400

    removed stopwords condition and unnecessary comments

--- yelper.py
+++ yelper.py
@@ -4,29 +4,13 @@ import operator
 import re
 import sys
 
-from nltk.corpus import stopwords
-
 word_list = []
-
-
-# Set up a quick lookup table for common words like "the" and "an" so they can be excluded
-
-nltk.download('stopwords')
-stops = set(stopwords.words('english'))
 
 with open("review.json") as myfile:
     head = [next(myfile) for x in range(10000)]
 
 word_list = re.sub("[^\w]", " ",  ' '.join(head)).split()
 cleaned_words = [w.lower() for w in word_list if w.isalnum()]
-
-
-# print(head)
-
-# with open('review.json') as f:
-#     data = json.load(f)
-
-# print(data)
 
 
 def getsentence(root, n, forward_direction):
@@ -36,8 +20,7 @@ def getsentence(root, n, forward_direction):
 
     bgs = nltk.bigrams(cleaned_words)
 
-    sun_bigrams = [b for b in bgs if (b[1-forward_direction] == root)
-                   and b[0] not in stops and b[1] not in stops]
+    sun_bigrams = [b for b in bgs if (b[1-forward_direction] == root)]
 
     fdist = nltk.FreqDist(sun_bigrams)
 
