9489116db0da600ac9b0fe56df4b66f0e771da61
Jay C <jaytchheda@gmail.com>
2018-05-21 05:44:11 -0400

    cleaned code

--- yelper.py
+++ yelper.py
@@ -33,15 +33,18 @@ def getsentence(root, n, forward_direction):
         return getsentence(word, n-1, forward_direction) + ' ' + root
 
 
+# initial defaults
 start = "chocolate"
 n = 5
 forward_direction = 1
+
+# apply arguments
 if(len(sys.argv) > 1):
     start = sys.argv[1]
 if(len(sys.argv) > 2):
     n = int(sys.argv[2])
-
 if(len(sys.argv) > 3):
     forward_direction = int(sys.argv[3])
+
 
 print(getsentence(start, n, forward_direction))
